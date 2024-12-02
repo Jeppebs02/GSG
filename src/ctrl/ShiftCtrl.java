f23c62402531e55499dc8ccd850aea34304beeb4
Oscar Hermann <oscar.hermann94@gmail.com>
2024-12-02 11:07:20 +0100

    ShiftCntrl

--- src/ctrl/ShiftCtrl.java
+++ src/ctrl/ShiftCtrl.java
@@ -2,17 +2,31 @@ package ctrl;
 
 import java.time.LocalDateTime;
 
+import dal.DataAccessException;
+import model.Employee;
 import model.Shift;
 
 public class ShiftCtrl {
-
-	//TODO
-	public Shift addShift(LocalDateTime startTime, LocalDateTime expectedEndTime) {
-		return null;
+	private Shift currentShift;
+	private EmployeeCtrl ec;
+	
+	public ShiftCtrl() throws DataAccessException {
+		this.currentShift = null;
+		ec = new EmployeeCtrl();
 	}
 	
-	public void addEmployeeToShift(Shift shift, ) {
+	public Shift createShift(LocalDateTime startTime, LocalDateTime endTime) {
+		currentShift = new Shift(startTime, endTime);
 		
+		return currentShift;
+	}
+	
+	public void addEmployeeToShift(Shift shift, int employeeID) {
+		Employee e = ec.findEmployeeByID(employeeID);
+		if (e == null) {
+			throw new NullPointerException();
+		}
+		currentShift.setEmployee(e);
 	}
 	
 }
