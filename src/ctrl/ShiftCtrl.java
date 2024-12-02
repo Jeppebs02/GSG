f231e3074f0c75d26d29862bf140522fb89317d6
MatiasHolmNielsen <10519141@ucn.dk>
2024-12-02 09:47:33 +0100

    SHiftCtrl

--- src/ctrl/ShiftCtrl.java
+++ src/ctrl/ShiftCtrl.java
@@ -1,5 +1,18 @@
 package ctrl;
 
+import java.time.LocalDateTime;
+
+import model.Shift;
+
 public class ShiftCtrl {
 
+	//TODO
+	public Shift addShift(LocalDateTime startTime, LocalDateTime expectedEndTime) {
+		return null;
+	}
+	
+	public void addEmployeeToShift(Shift shift, ) {
+		
+	}
+	
 }
