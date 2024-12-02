d9d7d85b2abf4e15067f50c2e862c3b78f9d0118
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2024-12-02 15:23:33 +0100

    Created test and finished userDB

--- src/dal/UserDB.java
+++ src/dal/UserDB.java
@@ -3,7 +3,10 @@ package dal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
+
+import model.AccountPrivileges;
 import model.User;
 
 public class UserDB implements UserDBIF	{
@@ -12,40 +15,63 @@ public class UserDB implements UserDBIF	{
 	private Connection connection = DBConnection.getInstance().getConnection();
 	
 	private static final String find_customer_info_by_user_id =
-			"SELECT * FROM [User] WHERE ID = 1 AND Type IN ('CUSTOMER', 'GUEST');";
+			"SELECT * FROM [User] WHERE ID = ? AND Type IN ('CUSTOMER', 'GUEST');";
 	
-	PreparedStatement findCustomerIdByUserId;
+	PreparedStatement findCustomerInfoByUserId;
 
+	/**
+     * Creates and returns a User object based on a given userID.
+     *
+     * @return User
+     */
+	
 	@Override
 	public User findCustomerByUserID(int userID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	
-	
-	
-
-	public int findCustomerIDFromUserID(int userID) {
-		int employeeID = 0;
+		User user = null;
 		try {
-			findEmployeeIDByUserID = connection.prepareStatement(find_employee_id_by_user_id);
-			findEmployeeIDByUserID.setInt(1, userID);
-			ResultSet rs = dbConecction.getResultSetWithPS(findEmployeeIDByUserID);
+			findCustomerInfoByUserId = connection.prepareStatement(find_customer_info_by_user_id);
+			findCustomerInfoByUserId.setInt(1, userID);
+			ResultSet rs = dbConecction.getResultSetWithPS(findCustomerInfoByUserId);
 			
 			if(rs.next()) {
-				System.out.println("Result set is not empty");
-				
-				employeeID = rs.getInt("Employee_ID");
-				System.out.println(employeeID);
+				user=createUserFromResultSet(rs);
 			}
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-		return employeeID;
+		return user;
 	}
+	
+	
+	
+	//TODO implement address get functionality. 
+	
+	/**
+     * Creates and returns a User object based on information in a ResultSet.
+     *
+     * @return User
+     */
+	
+	public User createUserFromResultSet(ResultSet rs) throws SQLException {
+		
+		// Get fields from ResultSet
+        int userID = rs.getInt("ID"); // User ID
+        String userName = rs.getString("UserName");
+        String passWord = rs.getString("Password");
+        String firstName = rs.getString("FirstName");
+        String lastName = rs.getString("LastName");
+        String email = rs.getString("Email");
+        String phoneNr = rs.getString("PhoneNr");
+        String address = ""; // Placeholder, `Address` is not in result set
+        AccountPrivileges accountPrivileges = AccountPrivileges.valueOf(rs.getString("AccountPrivileges"));
+
+        // Create and return the User object
+        User user = new User(userName, passWord, firstName, lastName, email, phoneNr, address, accountPrivileges);
+        user.setUserID(userID); // Set the user ID after creation of object
+        return user;
+    }
+		
+	
 
 }
---
+++ test/dal/UserDBTest.java
@@ -0,0 +1,58 @@
+package dal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.User;
+
+class UserDBTest {
+	UserDB udb;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testUserDB() {
+		udb = new UserDB();
+		
+		User usr = udb.findCustomerByUserID(2);
+		
+		System.out.println("user email is: "+usr.getEmail());
+		
+		assertNotNull(usr);
+		
+		
+		
+	}
+	
+	@Test
+	public void testConnection() {
+		try {
+			Connection c = DBConnection.getInstance().getConnection();
+			assertNotNull(c);
+		} catch (Exception e) {
+			fail("Issues with connection");
+		}
+	}
+
+}
