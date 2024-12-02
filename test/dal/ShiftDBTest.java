cee651471fa63c6ae2fc567827bb2fd5f261258a
Oscar Hermann <oscar.hermann94@gmail.com>
2024-12-02 12:45:13 +0100

    testclass prep

--- test/dal/ShiftDBTest.java
+++ test/dal/ShiftDBTest.java
@@ -1,37 +1,27 @@
-/**
- * 
- */
-package model;
+package dal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * 
- */
-class JUnitTestTestClass {
+class ShiftDBTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@AfterEach
 	void tearDown() throws Exception {
 	}
