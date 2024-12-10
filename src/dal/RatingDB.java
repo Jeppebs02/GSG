cd06ee1a00b5b0242b1998ea7ce458076e49bb38
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2024-12-10 10:20:36 +0100

    Rating

--- src/dal/RatingDB.java
+++ src/dal/RatingDB.java
@@ -1,5 +1,62 @@
 package dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Rating;
+import model.Report;
+
 public class RatingDB implements RatingDBIF {
+	
+	private Connection connection;
+	private DBConnection dbConnection;
+	
+	private static final String create_rating = "INSERT INTO [Rating] (SecurityScore,SecurityComment,ServiceScore,ServiceComment,ReportNr,Employee_ID) VALUES(?,?,?,?,?,?);";
+	
+	private PreparedStatement createRating;
+	
+
+	public RatingDB() throws SQLException {
+		super();
+		connection = DBConnection.getInstance().getConnection();
+		dbConnection = DBConnection.getInstance();
+		
+		createRating = connection.prepareStatement(create_rating);
+	}
+
+	@Override
+	public void saveRatingToDB(Rating rating, int reportNr) throws Exception {
+		int ratingID;
+		createRating.setInt(1, rating.getSecurityScore());
+		createRating.setString(2, rating.getSecurityComment());
+		createRating.setInt(3, rating.getServiceScore());
+		createRating.setString(4, rating.getServiceComment());
+		createRating.setInt(5, reportNr);
+		createRating.setInt(6, rating.getEmployee().getEmployeeID());
+		
+		
+		 try {
+	            // Execute and retrieve generated key
+	            ratingID = dbConnection.executeSqlInsertWithIdentityPS(createRating);
+	            task.setTaskID(taskID);
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+		
+		
+	}
+
+	@Override
+	public void deleteRatingByRatingID(int ratingID) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Rating findRatingByReportID(int reportID) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/dal/RatingDBIF.java
+++ src/dal/RatingDBIF.java
@@ -1,10 +1,16 @@
 package dal;
 
+import java.sql.Connection;
+
 import model.Rating;
+import model.Report;
 
 public interface RatingDBIF {
+
 	
-	public void saveRatingToDB() throws Exception;
-	public Rating updateRating() throws Exception;
+	
+	public void saveRatingToDB(Rating rating, int reportNr) throws Exception;
+	public void deleteRatingByRatingID(int ratingID) throws Exception;
+	public Rating findRatingByReportID(int reportID) throws Exception;
 
 }
