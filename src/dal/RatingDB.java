9a4bd8dd614c542154ce2564b61601323e6479ec
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2024-12-10 10:46:43 +0100

    more rating and a fix in rating

--- src/dal/RatingDB.java
+++ src/dal/RatingDB.java
@@ -2,6 +2,7 @@ package dal;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -14,28 +15,26 @@ public class RatingDB implements RatingDBIF {
 	private Connection connection;
 	private DBConnection dbConnection;
 	
-	private static final String create_rating = "INSERT INTO [Rating] (SecurityScore,SecurityComment,ServiceScore,ServiceComment,ReportNr,Employee_ID) VALUES(?,?,?,?,?,?);";
-	private static final String
+	private static final String save_rating = "INSERT INTO [Rating] (SecurityScore,SecurityComment,ServiceScore,ServiceComment,ReportNr,Employee_ID) VALUES(?,?,?,?,?,?);";
+	private static final String delete_rating_by_rating_id = "DELETE FROM [Rating] WHERE ID = ?;";
 	
-	private PreparedStatement createRating;
-	
+	private PreparedStatement saveRating;
+	private PreparedStatement deleteRatingByRatingID;
 
 	public RatingDB() throws SQLException {
 		super();
 		connection = DBConnection.getInstance().getConnection();
 		dbConnection = DBConnection.getInstance();
 		
-		createRating = connection.prepareStatement(create_rating);
+		saveRating = connection.prepareStatement(save_rating);
+		deleteRatingByRatingID = connection.prepareStatement(delete_rating_by_rating_id);
 	}
 
 	
 	
 	
 	 /**
-     * Retrieves all tasks from the database that fall within a specified month and year.
-     * 
-     * <p>This method prepares a SELECT query using the given year and month, executes it, 
-     * and constructs a list of {@link Task} objects from the result set.</p>
+     * Saves a rating object to database.
      * 
      * @param rating - a rating object you want to save.
      * @param reportNr - the reportNr of the report you want to save the rating to.
@@ -45,17 +44,17 @@ public class RatingDB implements RatingDBIF {
 	@Override
 	public Rating saveRatingToDB(Rating rating, int reportNr) throws Exception {
 		int ratingID;
-		createRating.setInt(1, rating.getSecurityScore());
-		createRating.setString(2, rating.getSecurityComment());
-		createRating.setInt(3, rating.getServiceScore());
-		createRating.setString(4, rating.getServiceComment());
-		createRating.setInt(5, reportNr);
-		createRating.setInt(6, rating.getEmployee().getEmployeeID());
+		saveRating.setInt(1, rating.getSecurityScore());
+		saveRating.setString(2, rating.getSecurityComment());
+		saveRating.setInt(3, rating.getServiceScore());
+		saveRating.setString(4, rating.getServiceComment());
+		saveRating.setInt(5, reportNr);
+		saveRating.setInt(6, rating.getEmployee().getEmployeeID());
 		
 		
 		 try {
 	            // Execute and retrieve generated key
-	            ratingID = dbConnection.executeSqlInsertWithIdentityPS(createRating);
+	            ratingID = dbConnection.executeSqlInsertWithIdentityPS(saveRating);
 	            rating.setRatingID(ratingID);
 	        } catch (SQLException e) {
 	            e.printStackTrace();
@@ -63,10 +62,22 @@ public class RatingDB implements RatingDBIF {
 		 
 		 return rating;
 	}
-
+	
+	 /**
+     * Deletes a rating object from the database.
+     * 
+     * @param ratingID - the ID of the rating you want to delete.
+     * @return void.
+     * @throws Exception.
+     */
 	@Override
 	public void deleteRatingByRatingID(int ratingID) throws Exception {
-		// TODO Auto-generated method stub
+		deleteRatingByRatingID.setInt(1, ratingID);
+		try {
+			dbConnection.executeSqlInsertWithIdentityPS(deleteRatingByRatingID);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -77,6 +88,18 @@ public class RatingDB implements RatingDBIF {
 	public List<Rating> findRatingsByReportID(int reportID) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+
+
+	@Override
+	public Rating createRatingFromResultSet(ResultSet rs) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Rating rating;
+		return rating = new Rating(rs.getInt("SecurityScore"), rs.getString("SecurityComment"), 0, null, null);
+		
 	}
 
 
