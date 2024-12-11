8c1a8ec3a802db8ac5d1974572a2d2c3ded9d847
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2024-12-11 13:16:49 +0100

    Update ReportDB.java

--- src/dal/ReportDB.java
+++ src/dal/ReportDB.java
@@ -141,7 +141,7 @@ public class ReportDB implements ReportDBIF {
 			updateReportByTaskID.setString(6, customerSignature);
 			updateReportByTaskID.setInt(7, taskID);
 			
-			dbConnection.executeSqlInsertWithIdentityPS(updateReportByTaskID);
+			dbConnection.getResultSetWithPS(updateReportByTaskID);
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
