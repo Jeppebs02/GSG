2c53caf05dbea5ea27709c4a1af2f4996ff3e1b3
Oscar Hermann <oscar.hermann94@gmail.com>
2024-12-11 10:07:46 +0100

    checks in Shift
    startTime vs. endTime

--- src/model/Shift.java
+++ src/model/Shift.java
@@ -3,52 +3,57 @@ package model;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import java.time.LocalDateTime;
+
 public class Shift {
-	private LocalDateTime startTime;
-	private LocalDateTime endTime;
-	private Employee employee;
-	private int shiftID;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Employee employee;
+    private int shiftID;
 
+    public Shift(LocalDateTime startTime, LocalDateTime endTime) {
+        if (startTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("Start time cannot be later than end time.");
+        }
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
-	public Shift(LocalDateTime startTime, LocalDateTime endTime) {
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
-	
-	public LocalDateTime getStartTime() {
-		return startTime;
-	}
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
 
+    public void setStartTime(LocalDateTime startTime) {
+        if (endTime != null && startTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("Start time cannot be later than end time.");
+        }
+        this.startTime = startTime;
+    }
 
-	public void setStartTime(LocalDateTime startTime) {
-		this.startTime = startTime;
-	}
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
 
+    public void setEndTime(LocalDateTime endTime) {
+        if (startTime != null && endTime.isBefore(startTime)) {
+            throw new IllegalArgumentException("End time cannot be earlier than start time.");
+        }
+        this.endTime = endTime;
+    }
 
-	public LocalDateTime getEndTime() {
-		return endTime;
-	}
+    public Employee getEmployee() {
+        return employee;
+    }
 
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 
-	public void setEndTime(LocalDateTime endTime) {
-		this.endTime = endTime;
-	}
+    public int getShiftID() {
+        return shiftID;
+    }
 
-	
-	public Employee getEmployee() {
-		return employee;
-	}
-
-	
-	public void setEmployee(Employee employee) {
-		this.employee = employee;
-	}
-	
-	public int getShiftID() {
-		return shiftID;
-	}
-	
-	public void setShiftID(int shiftID) {
-		this.shiftID = shiftID;
-	}
+    public void setShiftID(int shiftID) {
+        this.shiftID = shiftID;
+    }
 }
